9db8974fa7f61bc6cdf618845348420ae61b51c3
Matthew Tole <matthewtole@gmail.com>
2018-11-15 00:27:12 -0800

    :sparkles: Build out a super basic landing page

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import {StaticQuery, graphql} from 'gatsby';
 
-import Header from './header';
+import './normalize.css';
 import './layout.css';
 
 const Layout: React.SFC = ({children}) => (
@@ -27,18 +27,13 @@ const Layout: React.SFC = ({children}) => (
           ]}
         >
           <html lang="en" />
+          <link
+            href="https://fonts.googleapis.com/css?family=Roboto:100"
+            rel="stylesheet"
+          />
+          >
         </Helmet>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
-          {children}
-        </div>
+        {children}
       </>
     )}
   />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,20 @@ import React from 'react';
 
 import Layout from '../components/layout';
 
-const IndexPage = () => <Layout />;
+const IndexPage = () => (
+  <Layout>
+    <div className="boxes">
+      <div className="box box--matthew">
+        <a href="http://www.matthewtole.com">Matthew</a>
+      </div>
+      <div className="box box--katherine">
+        <a href="https://katherinetole.com">Katherine</a>
+      </div>
+      <div className="box box--wedding">
+        <a href="https://wedding.kathew.us">Wedding</a>
+      </div>
+    </div>
+  </Layout>
+);
 
 export default IndexPage;
